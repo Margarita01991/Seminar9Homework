6f6cf9e0e0f15782404c833096bcffc563628341
Margarita <margarita27011991@gmail.com>
2022-09-03 14:56:41 +0300

    вычисления функции Аккермана с помощью рекурсии

---
+++ task66/Program.cs
@@ -0,0 +1,23 @@
+﻿/* ЗЗадайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30 */
+Console.WriteLine("Введите положительное число: ");
+int numberM = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите положительное число: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+int SumNumbers(int numberM, int numberN)
+{
+    if (numberM == numberN)
+    {
+        return numberM;
+    }
+    else if (numberM > numberN)
+    {
+        return numberM + SumNumbers(numberM - 1, numberN);
+    }
+    else
+    {
+        return numberM + SumNumbers(numberM + 1, numberN);
+    }
+}
+Console.WriteLine(SumNumbers(numberM, numberN));
