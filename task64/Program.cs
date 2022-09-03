1411b313fd51dc72942ca2906d50218e99a621a2
Margarita <margarita27011991@gmail.com>
2022-09-03 13:27:07 +0300

    вывод всех натуральных чисел в промежутке от N до 1

--- task64/Program.cs
+++ task64/Program.cs
@@ -1,18 +1,19 @@
 ﻿/* Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1.
 N = 5 -> "5, 4, 3, 2, 1"
 N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1" */
+
 Console.Clear();
 void PrintNumber(int numberN, int numberM)
 {
-    if (numberM == numberN)
-    {
-        Console.Write(numberN + " ");
-        return;
-    }
-    else if (numberM > numberN)
+    if (numberM > numberN)
     {
         PrintNumber(numberN + 1, numberM);
         Console.Write(numberN + " ");
+    }
+    else if (numberN == numberM)
+    {
+        Console.Write(numberN + " ");
+        return;
     }
     else
     {
